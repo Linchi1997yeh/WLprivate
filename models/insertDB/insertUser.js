5a30164a2677871f9dbadfa66edecf6c6526ed72
Lin Chi Yeh <linchi1997yeh@gmail.com>
2020-01-13 11:59:19 +0800

    db user add default position

--- models/insertDB/insertUser.js
+++ models/insertDB/insertUser.js
@@ -6,31 +6,36 @@ var user1 = {
     username: "餅軍",
     email: "test1@gmail.com",
     password: newUser.generateHash("123") ,
-    houseName: "半伴西門"
+    houseName: "半伴西門",
+    position:""
 };
 var user2 = {
     username: "test2",
     email: "test2@gmail.com",
     password: newUser.generateHash("234"),
-    houseName: "半伴敦南"
+    houseName: "半伴敦南",
+    position:""
 };
 var user3 = {
     username: "test3",
     email: "test3@gmail.com",
     password: newUser.generateHash("345"),
-    houseName: "半伴北車"
+    houseName: "半伴北車",
+    position:""
 };
 var user4 = {
     username: "test4",
     email: "test4@gmail.com",
     password: newUser.generateHash("456"),
-    houseName: "半伴西門"
+    houseName: "半伴西門",
+    position:""
 };
 var user5 = {
     username: "test5",
     email: "test5@gmail",
     password: newUser.generateHash("567"),
-    houseName: "半伴北車"
+    houseName: "半伴北車",
+    position:""
 };
 var user6 = {
     username: "小管家-有緣",
